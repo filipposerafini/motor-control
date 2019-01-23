c4d16362ada820056f46582b7ea1791977c5b123
Angelo D'Aloia <angelo.daloia@yahoo.com>
2019-01-23 19:51:47 +0100

    MATLAB: started uC code on Simulink
    implemented encoder and current sensor readings.

--- MATLAB/LQR_control_design.m
+++ MATLAB/LQR_control_design.m
@@ -6,9 +6,12 @@ K = 0.383; % [V/rad/s] Back electromotive force coefficient
 I = 0.0021; % [Kg*m^2] Equivalent moment of inertia
 B = 0.0088; % [N*m*s/rad] Equivalent viscous damping coefficient
 
-%sensors (boolean: 1->measured, 0->not measured)
-%omega_meas = 1;
-%i_meas = 1;
+%sensors acquisition params
+PPR = 1000; % ENCODER pulses per revolution
+ADC_Vref = 3.3; % [V] reference voltage of the ADC
+ADC_bits = 12; % resolution (number of bits) of the adc
+PU_gain = 0.5; % current sensor conversion gain
+PU_offset = 1; % current sesnsor conversion offset
 
 %LQR tuning
 omega_max = 20; % [rad/sec] maximum admitted angular speed
@@ -17,8 +20,8 @@ int_max = 2; % integral error maximum
 v_max = 10; % [V] maximum admitted input voltage
 
 %linsys matrices_____________________________________________________
-%---| x = Ax + B1u |-------------------------------------------------
-%---| y = Cx       |-------------------------------------------------
+%---| x = Ax + Bu |--------------------------------------------------
+%---| y = Cx      |--------------------------------------------------
 A = [-B/I K/I; -K/L -R/L];
 B = [0; 1/L];
 C = [1 0];
@@ -35,4 +38,3 @@ R = inv([(v_max)^2]); % input penalization
 sys = ss(A,B,C,0);
 [K,S,e] = lqi(sys,Q,R); % gain K computation
 K = -K;
-%G = 1/(C*inv(A+B*K)*B); % trajectory tracking gain
