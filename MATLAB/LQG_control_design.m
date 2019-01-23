e774495c71ee8e9d50eebcf5583268503c9bbdd5
Angelo D'Aloia <angelo.daloia@yahoo.com>
2019-01-23 17:54:34 +0100

    MATLAB: implemented lqi controller
    lqi is an lqr with output reference tracking

--- MATLAB/LQG_control_design.m
+++ MATLAB/LQG_control_design.m
@@ -7,19 +7,32 @@ I = 0.0021; % [Kg*m^2] Equivalent moment of inertia
 B = 0.0088; % [N*m*s/rad] Equivalent viscous damping coefficient
 
 %sensors (boolean: 1->measured, 0->not measured)
-omega_meas = 1;
-i_meas = 0;
+%omega_meas = 1;
+%i_meas = 1;
+
+%LQR tuning
+omega_max = 20; % [rad/sec] maximum admitted angular speed
+i_max = 20; % [A] maximum admitted current
+int_max = 2; % integral error maximum
+v_max = 10; % [V] maximum admitted input voltage
 
 %linsys matrices_____________________________________________________
 %---| x = Ax + B1u |-------------------------------------------------
 %---| y = Cx       |-------------------------------------------------
 A = [-B/I K/I; -K/L -R/L];
-B1 = [0; 1/L];
-C = [omega_meas 0; 0 i_meas];
+B = [0; 1/L];
+C = [1 0];
 
-if(rank(ctrb(A,B1))~=2) % reachability check
+if(rank(ctrb(A,B))~=2) % reachability check
     disp("system not fully reachable");
     %perform some actions (e.g. led blinking)
 else disp("system fully reachable");    
 end    
    
+%LQR controller______________________________________________________
+Q = 1/2*inv([(omega_max)^2 0 0; 0 (i_max)^2 0; 0 0 (int_max)^2]); % state penalization
+R = inv([(v_max)^2]); % input penalization
+sys = ss(A,B,C,0);
+[K,S,e] = lqi(sys,Q,R); % gain K computation
+K = -K;
+%G = 1/(C*inv(A+B*K)*B); % trajectory tracking gain
